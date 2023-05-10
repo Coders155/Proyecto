07b447da9c82f21a735a51e8b3f4e68f7e3d15a2
CarlosMaxAS <133040615+CarlosMaxAS@users.noreply.github.com>
2023-05-10 00:02:33 -0600

    Add files via upload

---
+++ Triangulo Escaleno.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+print ("TRIANGULO ESCALENO")
+
+ba = int (input("Inserte cm de Base: "))
+a = int (input("Inserte cm de Lado A: "))
+
+#El primero es para Este y Oeste
+#El segundo es para Norte y Sur
+
+ax.plot([0,ba], [0,0])##Base
+ax.plot([0,ba], [0,a])##Lado A
+ax.plot([ba, ba], [a,0])##Lado B
+plt.show()
