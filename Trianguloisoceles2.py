40f8dace54376a1f6b3d74adb7e2fa3df10c7f9a
Julatl05 <133040269+Julatl05@users.noreply.github.com>
2023-05-26 19:33:15 -0600

    Create Trianguloisoceles2.py
    Triangulo isoceles final

---
+++ Trianguloisoceles2.py
@@ -0,0 +1,7 @@
+print("considera que el triangulo isoceles tiene los lados mas altos que la base")
+A=int(input("Escribe la altura: "))
+B=int(input("Escribe la base: "))
+s=0
+for i in range (A):
+    espacios=A-i
+    print(" "*espacios+"* "*i)
