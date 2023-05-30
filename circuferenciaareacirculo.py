4fbacd2d329a885b81694de841cb04a71ae62399
Julatl05 <133040269+Julatl05@users.noreply.github.com>
2023-05-30 11:18:40 -0600

    Create circuferenciaareacirculo.py

---
+++ circuferenciaareacirculo.py
@@ -0,0 +1,7 @@
+radio = float(input("Ingresa el radio del círculo: "))
+
+area = math.pi * radio ** 2
+circunferencia = 2 * math.pi * radio
+
+print("El área del círculo es:", area)
+print("La circunferencia del círculo es:", circunferencia)
