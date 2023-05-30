740f852122411eebe760ac7735c347449c75a495
Julatl05 <133040269+Julatl05@users.noreply.github.com>
2023-05-30 10:47:25 -0600

    Create perisemiperiarea2.py
    perimetro y eso

---
+++ perisemiperiarea2.py
@@ -0,0 +1,6 @@
+perimetro = a + b + c
+s = (a + b + c) / 2
+area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+print("El perímetro del triángulo es:", perimetro)
+print("El semiperímetro del triángulo es:", s)
+print("El área del triángulo es:", area)
