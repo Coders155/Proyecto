e03fda086fe17a91701cf2ba6262be0601192a2a
Julatl05 <133040269+Julatl05@users.noreply.github.com>
2023-05-30 10:55:32 -0600

    Create periarearomboide.py
    romboide

---
+++ periarearomboide.py
@@ -0,0 +1,9 @@
+bas = float(input("Ingresa la longitud de la base del romboide: "))
+alt = float(input("Ingresa la altura del romboide: "))
+lad = float(input("Ingresa la longitud de uno de los lados del romboide: "))
+
+area = bas * alt
+perimetro = 2 * (bas + lad)
+
+print("El área del romboide es:", area)
+print("El perímetro del romboide es:", perimetro)
