025b772c1b45f6b36e5000c0931706da075f9295
Julatl05 <133040269+Julatl05@users.noreply.github.com>
2023-05-30 11:02:40 -0600

    Create periareahexagon.py
    hexagono

---
+++ periareahexagon.py
@@ -0,0 +1,7 @@
+lado = float(input("Ingresa la longitud del lado del hexágono: "))
+
+area = (3 * (3 ** 0.5) * lado ** 2) / 2
+perimetro = 6 * lado
+
+print("El área del hexágono es:", area)
+print("El perímetro del hexágono es:", perimetro)
