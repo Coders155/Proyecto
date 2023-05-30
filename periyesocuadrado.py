c8e12e0d27b986fcfa06ca0b1674256a287e9e57
Julatl05 <133040269+Julatl05@users.noreply.github.com>
2023-05-30 10:49:49 -0600

    Create periyesocuadrado.py
    cuadrdo

---
+++ periyesocuadrado.py
@@ -0,0 +1,7 @@
+lado = float(input("Ingresa la longitud del lado del cuadrado: "))
+
+area = lado ** 2
+perimetro = 4 * lado
+
+print("El área del cuadrado es:", area)
+print("El perímetro del cuadrado es:", perimetro)
